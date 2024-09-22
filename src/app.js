b80114b67178bf8b39c6e3924b11ba7dc1b2f5a1
Piotrek <Piotr.karys96@gmail.com>
2024-09-23 01:46:30 +0200

    Revert "no ejs"
    This reverts commit a44ec43fca6afab0a4ae901454d492ea0dcfd41d.

--- src/app.js
+++ src/app.js
@@ -10,10 +10,12 @@ const path = require("path");
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "page"));
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "page")));
 app.use(passport.initialize());
 
 app.use("/", pageRoutes);
