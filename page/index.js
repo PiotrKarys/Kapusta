117f11f79ce06e11dcdf0d8d60e8eb79f94da1fa
Piotrek <Piotr.karys96@gmail.com>
2024-09-23 01:05:14 +0200

    counter

---
+++ page/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const VisitorCounter = require("../src/models/visitorCounterModel");
+
+router.get("/", async (req, res) => {
+  try {
+    let counter = await VisitorCounter.findOne();
+    if (!counter) {
+      counter = new VisitorCounter();
+    }
+    counter.count += 1;
+    await counter.save();
+
+    res.render("index", { visitorCount: counter.count });
+  } catch (error) {
+    console.error("Błąd przy aktualizacji licznika:", error);
+    res.status(500).send("Wystąpił błąd serwera");
+  }
+});
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -4,17 +4,21 @@ const cors = require("cors");
 const passport = require("../config/passport");
 const errorHandler = require("./middleware/errorMiddleware");
 const authRoutes = require("./routes/authRoutes");
+const pageRoutes = require("../page");
+const path = require("path");
 
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-app.get("/", (req, res) => {
-  res.send("Serwer działa poprawnie!");
-});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../page"));
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(passport.initialize());
 
+app.use("/", pageRoutes);
 app.use("/auth", authRoutes);
 app.use(errorHandler);
 
