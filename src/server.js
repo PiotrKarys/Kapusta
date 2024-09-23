88451927a41c26f12beb4fbf4b371e33f2d54d01
Piotrek <Piotr.karys96@gmail.com>
2024-09-23 04:21:56 +0200

    kapucha

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("../config/db");
+const colors = require("colors");
 
 const port = process.env.PORT || 3000;
 
@@ -10,7 +11,8 @@ async function startServer() {
     console.log("Connected to the database");
 
     const server = app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+      console.log(`Server is running on port ${port}`.blue);
+      console.log(`Access it at: http://localhost:${port}`.yellow);
     });
 
     server.setTimeout(45000);
