523aba8c64241259f7896228dc6dc87dd4633801
Piotrek <Piotr.karys96@gmail.com>
2024-09-27 16:36:27 +0200

    no swagger yet

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -5,10 +5,16 @@ const registerSchema = Joi.object({
     "string.email": "Please use a valid email address",
     "any.required": "Email is required",
   }),
-  password: Joi.string().min(6).required().messages({
-    "string.min": "Password must be at least 6 characters long",
-    "any.required": "Password is required",
-  }),
+  password: Joi.string()
+    .min(6)
+    .pattern(/^(?=.*[A-Za-z])(?=.*\d)/)
+    .required()
+    .messages({
+      "string.min": "Password must be at least 6 characters long",
+      "string.pattern.base":
+        "Password must contain at least one letter and one number",
+      "any.required": "Password is required",
+    }),
 });
 
 const loginSchema = Joi.object({
