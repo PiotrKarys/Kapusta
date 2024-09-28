2f49bf4cd4a7fe278ecd992b7662ec7c9a0c9cac
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2024-09-28 17:40:33 +0200

    add deleteTransaction
    dodano usuwanie transakcji, usunięcie transakcji aktualizuje saldo

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -4,6 +4,7 @@ const getExpenses = require("../controller/transactions/getExpenses");
 const addExpense = require("../controller/transactions/addExpense");
 const getIncomes = require("../controller/transactions/getIncomes");
 const addIncome = require("../controller/transactions/addInome");
+const deleteTransaction = require("../controller/transactions/deleteTransaction");
 const getTransactionsPeriodData = require("../controller/transactions/periodData");
 const authMiddleware = require("../middleware/authMiddleware");
 
@@ -14,6 +15,8 @@ router.post("/expense", authMiddleware, addExpense);
 router.get("/income", authMiddleware, getIncomes);
 
 router.post("/income", authMiddleware, addIncome);
+
+router.delete("/:transactionId", authMiddleware, deleteTransaction);
 
 router.get("/period-data", authMiddleware, getTransactionsPeriodData);
 module.exports = router;
