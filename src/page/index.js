880ccde972b4402d6e5336c73bdc8ab9b72ee7ae
Piotrek <Piotr.karys96@gmail.com>
2024-09-29 13:25:06 +0200

    add status feature

--- src/page/index.js
+++ src/page/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
 router.get("/", (req, res) => {
-  res.send("Server is operational and functioning correctly.");
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+router.get("/api/status", (req, res) => {
+  res.json({ message: "Serwer Kapu$ta działa poprawnie" });
 });
 
 module.exports = router;
